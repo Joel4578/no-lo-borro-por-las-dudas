20ad07827b6751241b821ad47323a22b56014139
NicoGGomez <anelecarg@icloud.com>
2024-10-29 12:26:06 -0300

    adelantamos algunas cosas del js

--- TP2/EntregaFinal/scripts/cuatroEnLinea.js
+++ TP2/EntregaFinal/scripts/cuatroEnLinea.js
@@ -15,21 +15,37 @@ const btnMultijugador = document.getElementById('multijugador');
 const btnOpciones = document.getElementById('btn-opciones');
 const pantallaOpciones = document.getElementById('pantalla-juego-opciones');
 
+// pantalla jugar solo
+
+const pantallaSolo = document.getElementById('pantalla-juego-solo');
+
 // botones opciones
 
 const opcionUno = document.getElementById('opcion-1');
 const opcionDos = document.getElementById('opcion-2');
 const volverAtras = document.getElementById('volver');
 
+// mostrar inicio de juego
+
 play.addEventListener('click', () =>{
     pantalla.style.display = 'none';
     pantallaActiva.style.display = 'flex';
- });
+});
+
+// jugar solo 
+btnJugarSolo.addEventListener('click', () =>{
+    pantallaActiva.style.display = 'none';
+    pantallaActiva.style.display = 'flex';
+}); 
+
+// abrir opciones de juego
 
 btnOpciones.addEventListener('click', () => {
     pantallaActiva.style.display = 'none';
-    pantallaOpciones.style.display = 'flex';
+    pantallaJue.style.display = 'flex';
 })
+
+// volver atras 
 
 volverAtras.addEventListener('click', () => {
     pantallaOpciones.style.display = 'none';
