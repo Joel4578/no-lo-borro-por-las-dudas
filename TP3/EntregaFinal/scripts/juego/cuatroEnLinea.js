ec946d846b94e3d16f3a30bb822a4fdd7feaea2e
NicoGGomez <anelecarg@icloud.com>
2024-10-29 16:26:49 -0300

    juego adelanto

---
+++ TP3/EntregaFinal/scripts/juego/cuatroEnLinea.js
@@ -0,0 +1,54 @@
+// opciones de debajo
+
+const pantallaGrande = document.getElementById('btn-fullscreen');
+
+// pantalla inactiva
+
+const play = document.getElementById('btn-play');
+const pantalla = document.getElementById('pantalla-juego');
+const pantallaPrincipal = document.getElementById('pantalla-juego-principal');
+
+// botones pantalla principal
+
+const btnJugarSolo = document.getElementById('solo');
+const btnMultijugador = document.getElementById('multijugador');
+
+// pantalla jugar solo
+
+const pantallaSolo = document.getElementById('pantalla-juego-solo');
+const volverAtras = document.getElementById('volver');
+const cuatroFichas = document.getElementById('cuatroPorCuatro');
+const cincoFichas = document.getElementById('cincoPorCinco');
+const seisFichas = document.getElementById('seisPorSeis');
+
+// seleccionar fichas
+
+const pantallaSeleccionarFicha = document.getElementById('pantalla-seleccionar-fichas');
+
+// mostrar inicio de juego
+
+play.addEventListener('click', () =>{
+    pantalla.style.display = 'none';
+    pantallaPrincipal.style.display = 'flex';
+});
+
+// jugar solo 
+btnJugarSolo.addEventListener('click', () =>{
+    pantallaPrincipal.style.display = 'none';
+    pantallaSolo.style.display = 'flex';
+}); 
+
+// cuatro por cuatro (facil)
+
+cuatroFichas.addEventListener('click', () => {
+    pantallaSolo.style.display = 'none';
+    pantallaSeleccionarFicha.style.display = 'flex';
+})
+
+// volver atras 
+
+volverAtras.addEventListener('click', () => {
+    pantallaSolo.style.display = 'none';
+    pantallaPrincipal.style.display = 'flex';
+})
+
